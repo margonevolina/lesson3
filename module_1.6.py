8d1d0542ebbbacdbd72d32edb7f7f021827a2cc4
margonevolina <margo6984@gmail.com>
2024-11-24 20:07:10 +0300

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,12 @@
+my_dict={"Margarita": 1992,"Kristina": 1986}
+print(my_dict)
+print(my_dict["Margarita"])
+print(my_dict.get("Maria"))
+my_dict.update({"Alena":1988,"Oxana":1975})
+print(my_dict.pop("Alena"))
+print(my_dict)
+my_set={3,3,1,5,"b","c","c",(13,31)}
+print(my_set)
+my_set.update([3.05,"apple"])
+my_set.discard(1)
+print(my_set)
